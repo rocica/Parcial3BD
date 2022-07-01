d481f45ab394d845b44c10f147c2b42a42f6efa2
rocica <105009559+rocica@users.noreply.github.com>
2022-06-30 20:43:03 -0500

    Update Problema1.sql
    Se cambio el nombre de la secuencia por id_auditoria

--- Problema1.sql
+++ Problema1.sql
@@ -1,4 +1,4 @@
-CREATE SEQUENCE sequence1
+CREATE SEQUENCE id_auditoria
     START WITH 1
     INCREMENT BY 1;
 
@@ -6,6 +6,6 @@ CREATE OR REPLACE TRIGGER insert_auditoria
 AFTER INSERT ON prestamo FOR EACH ROW
 BEGIN
 INSERT INTO auditoria (id_transaccion_au, tabla, id_cliente, cod_tipop, tipo_op, saldo_anterior, monto_pago, saldofinal, Usuario, Fecha)
-VALUES (sequence1.nextval, 'prestamo', :NEW.id_cliente, :NEW.cod_tipop, 'I', 0, 0,  :NEW.saldoactual,  user, sysdate ());
+VALUES (id_auditoria.nextval, 'prestamo', :NEW.id_cliente, :NEW.cod_tipop, 'I', 0, 0,  :NEW.saldoactual,  user, sysdate ());
 END insert_auditoria;
 /
