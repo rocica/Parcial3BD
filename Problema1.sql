c83c95f0a2d6a17a7f99dc99a99250af84953024
Rocio Cortez <rocio.cortez utp.ac.pa>
2022-06-30 20:31:12 -0500

    Archivo listo para pasar a mano

---
+++ Problema1.sql
@@ -0,0 +1,11 @@
+CREATE SEQUENCE sequence1
+    START WITH 1
+    INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER insert_auditoria
+AFTER INSERT ON prestamo FOR EACH ROW
+BEGIN
+INSERT INTO auditoria (id_transaccion_au, tabla, id_cliente, cod_tipop, tipo_op, saldo_anterior, monto_pago, saldofinal, Usuario, Fecha)
+VALUES (sequence1.nextval, 'prestamo', :NEW.id_cliente, :NEW.cod_tipop, 'I', 0, 0,  :NEW.saldoactual,  user, sysdate ());
+END insert_auditoria;
+/
